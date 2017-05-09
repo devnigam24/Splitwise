2df822b1343a9b3e2f5ab040bbdb3ba465c33f9b
Devvrat Nigam <devnigam24@gmail.com>
2017-05-09 11:33:55 -0700

    Removing Unwanted Files

--- splitwise-app/app/models/person.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  userEmail: DS.attr('string'),
-  userPassword: DS.attr('string')
-});
